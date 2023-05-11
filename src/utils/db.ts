6073a813b1a9e9660a4248368e923b5de52c3136
manafasif <manaf@utexas.edu>
2023-05-11 15:39:53 -0500

    fixed bugs

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 import { logger } from "./logger";
 import dotenv from "dotenv";
+import { Db } from 'mongodb';
+
 
 dotenv.config();
 
@@ -8,6 +10,8 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 if (!DB_USERNAME || !DB_PASSWORD) {
   logger.error("Error: No DB Credentials");
+  throw new Error("Error: No DB Credentials found. Check the .env file");
+
 }
 
 const url = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@hans-wehr.ujhfadm.mongodb.net/`;
@@ -15,11 +19,11 @@ const url = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@hans-wehr.ujhfadm.mongo
 // Database Name
 const dbName = "hans_wehr";
 
-let db;
+let db: Db;
 
 export async function initDB() {
   try {
-    let client = await MongoClient.connect(url, { useNewUrlParser: true });
+    let client = await MongoClient.connect(url);
     db = client.db(dbName);
     logger.debug("Successfully connected to MongoDB");
     return db;
@@ -28,7 +32,7 @@ export async function initDB() {
   } // catch any mongo error here
 }
 
-export async function lookupRoot(root) {
+export async function lookupRoot(root : string) {
   // connect to the collection
   let collection = await db.collection("definitions");
 
