dd80fb5f888a5349af8bfadabf0f58a141022e54
manafasif <manaf@utexas.edu>
2023-05-15 14:56:01 -0500

    added docker setup

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,15 @@ import swaggerUi from "swagger-ui-express";
 import { logger } from "./utils/logger";
 import { initDB } from "./utils/db";
 import { rootHandler } from "./routes/root";
-import { nounHandler }  from "./routes/noun";
-import { feedbackHandler }  from "./routes/feedback";
+import { nounHandler } from "./routes/noun";
+import { feedbackHandler } from "./routes/feedback";
 import { specs } from "./utils/swagger-api";
 
 const app = express();
 
 const PORT = process.env.PORT || 8080,
-  LOCAL = process.env.LOCAL || 0;
+  LOCAL = process.env.LOCAL || 0,
+  HTTP = process.env.HTTP || 0;
 
 var bodyParser = require("body-parser");
 
@@ -61,27 +62,39 @@ function startListening() {
     app.listen(PORT, () => {
       logger.info(`API listening port ${PORT}...`);
     });
-  } else {
-    const httpServer = http.createServer(app);
-    const httpsServer = https.createServer(
-      {
-        key: fs.readFileSync(
-          "/etc/letsencrypt/live/api.hanswehr.com/privkey.pem"
-        ),
-        cert: fs.readFileSync(
-          "/etc/letsencrypt/live/api.hanswehr.com/fullchain.pem"
-        ),
-      },
-      app
-    );
+  } else if (HTTP) {
+    initializeHTTP();
+  }
+  else {
+    initializeHTTP();
+    initializeHTTPS();
 
-    httpServer.listen(80, () => {
-      console.log("HTTP Server running on port 80");
-    });
-
-    httpsServer.listen(443, () => {
-      console.log("HTTPS Server running on port 443");
-    });
   }
 }
 
+// initializes the HTTP server
+function initializeHTTP() {
+  const httpServer = http.createServer(app);
+  httpServer.listen(80, () => {
+    console.log("HTTP Server running on port 80");
+  });
+}
+
+// initializes the HTTPS server
+function initializeHTTPS() {
+  const httpsServer = https.createServer(
+    {
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/api.hanswehr.com/privkey.pem"
+      ),
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/api.hanswehr.com/fullchain.pem"
+      ),
+    },
+    app
+  );
+
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
+}
