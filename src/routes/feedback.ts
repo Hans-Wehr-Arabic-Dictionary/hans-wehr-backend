18acda2cf7d292a0dc161a8b73fb3ab9ce813945
manafasif <manaf@utexas.edu>
2023-12-04 21:51:01 -0600

    email branch

--- src/routes/feedback.ts
+++ src/routes/feedback.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from "express";
 import { logger } from "../utils/logger";
 import { getFeedbackByRoot, getRecentFeeedback, insertFeedback } from "../utils/db";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+const EMAIL_LIST = ["manaf.asif12@gmail.com"];
 
 export interface Feedback {
   type: string;
@@ -10,9 +18,18 @@ export interface Feedback {
   message: string | undefined;
 }
 
+// Configure nodemailer transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL_USERNAME, // Replace with your Gmail email
+    pass: EMAIL_PASSWORD, // Replace with your Gmail password or an app-specific password
+  },
+});
+
 export const router = express.Router();
 
-router.post("/", (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
   const body: Feedback = req.body
   if (!("root" in body)) {
     logger.info("Recieved bad feedback request");
@@ -26,7 +43,19 @@ router.post("/", (req: Request, res: Response) => {
   }
   else {
     logger.info("Recieved feedback request", body["message"]);
-    insertFeedback(body);
+    await insertFeedback(body);
+
+    const subject = "Feedback Report";
+    const text = `New Feedback:\nType: ${body.type}\nName: ${body.name}\nEmail: ${body.email}\nRoot: ${body.root}\nMessage: ${body.message}`;
+
+    const mailOptions = {
+      from: EMAIL_USERNAME, // Replace with your Gmail email
+      to: EMAIL_LIST.join(", "),
+      subject,
+      text,
+    };
+
+    await transporter.sendMail(mailOptions);
     res.send("Thank you for your feedback, it has been processed");
   }
 })
